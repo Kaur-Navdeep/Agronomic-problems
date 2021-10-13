667c69f3e7548433220a11d682f88de3165c2858
navdeepkaurufl <NAVDEEPKAUR@UFL.EDU>
2021-10-13 14:45:21 -0400

    for_loop.R

--- for_loop.R
+++ for_loop.R
@@ -23,6 +23,7 @@ for(i in 5:ncol(data1)){
   column = names(data1[i])
   model1 = lm((data1[,i]~ data1$N_rates* Variety + Block), data = data1)
   anova = anova(model1)
+  HSD = HSD.test (model1, "N_rates", group = TRUE, console=TRUE)
   Shapiro = shapiro.test(model1$resid)
   print(column)
   print(anova)
@@ -30,6 +31,48 @@ for(i in 5:ncol(data1)){
   print(HSD)
 }
 sink()
+
+model_seed_yield = lm((Seed_yield~N_rates* Variety + Block), data = data1)
+anova_seed_yield = aov((Seed_yield~data1$N_rates* Variety + Block), data = data1); anova_seed_yield
+HSD_seed_yield = HSD.test(model, "N_rates"); HSD_seed_yield
+dev.off()  
+
+model_seed_yield = lm((Seed_yield~N_rates* Variety + Block), data = data1)
+anova_seed_yield = anova((model_seed_yield), data = data1); anova_seed_yield
+HSD_seed_yield = HSD.test(model_seed_yield, "N_rates"); HSD_seed_yield
+dev.off()  
+
+tky = as.data.frame((HSD_seed_yield$means)); tky
+tky$N_rates <- c("0", "112","168", "224", "280", "56")
+tky
+
+library(ggplot2)
+
+ggplot(tky, aes(x = N_rates, y = Seed_yield, fill=N_rates))+
+  geom_bar(stat="identity",position=position_dodge(), color="black",  ## stat equal to identity means avde jehra yield ala result a ohi number use krna na k jive hunda bar graph ch count use krn ala)
+           width = 0.7)+
+  labs(y = "Yield (Kg/ha)", x = "N rates (Kg/ha)") + ggtitle("Tukey test - dynamite plot")+
+  scale_color_manual(values=c("#2ca02c","#d62728"))+
+  geom_errorbar(aes(ymin=Seed_yield-((std)/sqrt(4)), ymax=Seed_yield+((std)/sqrt(4))), width=.2,
+                position=position_dodge(0.9))+
+  theme_light() + ylim(c(0,7)) + 
+  geom_text(data=tky,
+            aes( y = Seed_yield + ((std)/sqrt(4)) + 0.5, label=c("b","a","a","a","a","a")),vjust=0)
+
+
+model_plant_height =  (lm((Plant_height~N_rates + Block), data = data1))
+
+model_plant_height = lm(Plant_height ~ N_rates*Variety + Block, data = data1)
+anova = aov(model_plant_height); anova
+summary(anova(model_plant_height))
+plot(model_plant_height,1:3)         ## test for normality
+library(agricolae)
+HSDdata <- HSD.test (model_plant_height, "N_rates")
+HSDdata[["groups"]]
+shapiro.test(model_plant_height$resid)
+
+
+
 summary = list()
   ## model using aov   
 for(i in 5:ncol(data1)){
@@ -47,9 +90,9 @@ anova = list()
 ## 
 for(i in 5:ncol(data1)){
   column = names(data1[i])
-  anova= aov(data1[,i]~ data1[,i]$N_rates + data1[,i]Variety + data1[,i]Block)
+  anova = aov(data1[,i]~ data1[,i]$N_rates + data1[,i]Variety + data1[,i]Block)
   Shapiro = shapiro.test(model1$resid)
-  HSD = HSD.test (model1, trt = c("N_rates", "Variety"), group = TRUE, console=TRUE)
+  HSD = HSD.test (anova, trt = c("N_rates", "Variety"), group = TRUE, console=TRUE)
   print(column)
   print(anova)
   print(Shapiro)
@@ -57,7 +100,7 @@ for(i in 5:ncol(data1)){
 }
 ## Extra
 data$
-  HSD = HSD.test (model1, trt = c("N_rates"), group = TRUE, console=TRUE)
+  HSD = HSD.test (model1, trt = "N_rates", group = TRUE, console=TRUE)
 
 df = df.residual(model1)
 MSerror = deviance(model1)/df
